d6a549cae52cbd122ccb060333c8ede2cb381bc1
RuslanSat <ruslansatvalidev1@gmail.com>
2025-03-29 12:33:54 +0500

    Task 2

---
+++ src/Task3.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int n = 10;
+    /*calcultaing time complexity*/
+        double startTime = System.nanoTime();
+        boolean isPrime = isPrime(n);
+        double endTime = System.nanoTime();
+
+        System.out.println(n + " is " + (isPrime ? "Prime" : "Composite"));
+        System.out.println("Time taken: " + (endTime - startTime) / 1_000_000.0 + " ms");
+    }
+    /*checking is the n is prime*/
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
